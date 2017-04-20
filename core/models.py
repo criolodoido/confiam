92823354cab46ba10df1af5991abc8947ad3a7f9
criolodoido <victor.santos.cd@gmail.com>
2017-04-19 22:15:43 -0300

    novas mudancas

--- core/models.py
+++ core/models.py
@@ -7,8 +7,13 @@ class Sobre(models.Model):
 	nome = models.CharField(max_length=200, null=False, blank=False)
 	apresentacao = models.TextField(null=False, blank=False)
 	foto = CloudinaryField('foto', null=True, blank=True)
+	created_date = models.DateTimeField(
+            default=timezone.now)
+	published_date = models.DateTimeField(
+            blank=True, null=True)
 
 	def publish(self):
+		self.published_date = timezone.now()
 		self.save()
 
 	def __str__(self):
@@ -18,19 +23,28 @@ class Palestras(models.Model):
 	nome_palestrante = models.CharField(max_length=200, null=False, blank=False)
 	sua_palestra = models.TextField(null=False, blank=False)
 	foto_palestrante = CloudinaryField('foto_palestra', null=True, blank=True)
-
+	created_date = models.DateTimeField(
+            default=timezone.now)
+	published_date = models.DateTimeField(
+            blank=True, null=True)
 	def publish(self):
+		self.published_date = timezone.now()
 		self.save()
 
 	def __str__(self):
-		return self.nome_palestra
+		return self.nome_palestrante
 
 class Minicursos(models.Model):
 	nome_minicurso = models.CharField(max_length=200, null=False, blank=False)
 	apresentacao_minicurso = models.TextField(null=False, blank=False)
 	foto_minicurso = CloudinaryField('foto_palestra', null=True, blank=True)
+	created_date = models.DateTimeField(
+            default=timezone.now)
+	published_date = models.DateTimeField(
+            blank=True, null=True)
 
 	def publish(self):
+		self.published_date = timezone.now()
 		self.save()
 
 	def __str__(self):
@@ -40,8 +54,13 @@ class Restaurantes(models.Model):
 	nome_restaurante = models.CharField(max_length=200, null=False, blank=False)
 	apresentacao_restaurante = models.TextField(null=False, blank=False)
 	foto_restaurante = CloudinaryField('foto_restaurante', null=True, blank=True)
+	created_date = models.DateTimeField(
+            default=timezone.now)
+	published_date = models.DateTimeField(
+            blank=True, null=True)
 
 	def publish(self):
+		self.published_date = timezone.now()
 		self.save()
 
 	def __str__(self):
@@ -51,8 +70,13 @@ class Patrocinio(models.Model):
 	nome_patrocinio = models.CharField(max_length=200, null=False, blank=False)
 	apresentacao_patrocinio = models.TextField(null=False, blank=False)
 	foto_patrocinio = CloudinaryField('foto_patrocinio', null=False, blank=False)
+	created_date = models.DateTimeField(
+            default=timezone.now)
+	published_date = models.DateTimeField(
+            blank=True, null=True)
 
 	def publish(self):
+		self.published_date = timezone.now()
 		self.save()
 
 	def __str__(self):
@@ -62,8 +86,13 @@ class Hoteis(models.Model):
 	nome_hotel = models.CharField(max_length=200, null=False, blank=False)
 	apresentacao_hotel = models.TextField(null=False, blank=False)
 	foto_hotel = CloudinaryField('foto', null=True, blank=True)
+	created_date = models.DateTimeField(
+            default=timezone.now)
+	published_date = models.DateTimeField(
+            blank=True, null=True)
 
 	def publish(self):
+		self.published_date = timezone.now()
 		self.save()
 
 	def __str__(self):
@@ -73,8 +102,13 @@ class Evento(models.Model):
 	nome_evento = models.CharField(max_length=200, null=False, blank=False)
 	apresentacao_evento = models.TextField(null=False, blank=False)
 	foto_evento = CloudinaryField('foto', null=True, blank=True)
+	created_date = models.DateTimeField(
+            default=timezone.now)
+	published_date = models.DateTimeField(
+            blank=True, null=True)
 
 	def publish(self):
+		self.published_date = timezone.now()
 		self.save()
 
 	def __str__(self):
--- core/views.py
+++ core/views.py
@@ -3,29 +3,29 @@ from django.utils import timezone
 from .models import Sobre, Palestras, Minicursos, Restaurantes, Patrocinio, Hoteis, Evento
 
 def index(request):
-	posts = Evento.objects.all()
+	posts = Evento.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 	return render(request, 'core/index.html', {'posts': posts})
 
 def sobre(request):
-	posts = Sobre.objects.all()
+	posts = Sobre.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'core/sobre.html', {'posts': posts})
 
 def palestras(request):
-	posts = Palestras.objects.all()
+	posts = Palestras.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'core/palestras.html', {'posts': posts})
 
 def minicursos(request):
-	posts = Minicursos.objects.all()
+	posts = Minicursos.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'core/minicursos.html', {'posts': posts})
 
 def restaurantes(request):
-	posts = Restaurantes.objects.all()
+	posts = Restaurantes.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'core/restaurantes.html', {'posts': posts})
 
 def patrocinio(request):
-	posts = Patrocinio.objects.all()
+	posts = Patrocinio.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'core/patrocinio.html', {'posts': posts})
 
 def hoteis(request):
-	posts = Hoteis.objects.all()
+	posts = Hoteis.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'core/hoteis.html', {'posts': posts})
